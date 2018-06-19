3d4ef1cdff685d05f9554463ad24f1fe675d27a6
Sankt Petersbug <sankt.petersbug@gmail.com>
2018-06-19 12:47:00 -0500

    splitted test to success/fail cases

--- internal/formatter/formatter_test.go
+++ internal/formatter/formatter_test.go
@@ -4,40 +4,24 @@ import (
 	"testing"
 )
 
-func TestContextFormat(t *testing.T) {
+func TestContextFormatSuccess(t *testing.T) {
 	testCases := []struct {
 		name          string
 		template      string
 		data          interface{}
-		errorExpected bool
-		expected      string
+		expected string
 	}{
 		{
 			name:          "Simple template",
 			template:      "Simple template with name {{.}}",
 			data:          "YO!",
-			errorExpected: false,
 			expected:      "Simple template with name YO!",
 		},
 		{
 			name:          "Template with tabs",
 			template:      "Template\twith\ttabs",
 			data:          nil,
-			errorExpected: false,
 			expected:      "Template            with                tabs",
-		},
-		{
-			name:          "Invalid template",
-			template:      "This template is not {{ valid",
-			errorExpected: true,
-			expected:      "",
-		},
-		{
-			name:          "Invalid data",
-			template:      "Template requires {{.Name}} Name attribute",
-			data:          "This does not have Name attribute",
-			errorExpected: true,
-			expected:      "",
 		},
 	}
 
@@ -46,12 +30,41 @@ func TestContextFormat(t *testing.T) {
 			c := Context{tc.name, tc.template}
 			result, err := c.Format(tc.data)
 
-			if (err != nil) != tc.errorExpected {
-				t.Errorf("expected error != nil: %v, err: %v", tc.errorExpected, err)
+			if err != nil {
+				t.Errorf("expected nil, but got an error %q", err)
 			}
 
 			if result != tc.expected {
 				t.Errorf("expected result: %v, saw: %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+func TestContextFormatFail(t *testing.T) {
+	testCases := []struct {
+		name          string
+		template      string
+		data          interface{}
+	}{
+		{
+			name:          "Invalid template",
+			template:      "This template is not {{ valid",
+		},
+		{
+			name:          "Invalid data",
+			template:      "Template requires {{.Name}} Name attribute",
+			data:          "This does not have Name attribute",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			c := Context{tc.name, tc.template}
+			_, err := c.Format(tc.data)
+
+			if err == nil {
+				t.Errorf("expected an error, but got nil")
 			}
 		})
 	}
