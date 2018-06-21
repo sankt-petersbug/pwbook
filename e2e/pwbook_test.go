67fd23f7070f52c7ad940cb2d82663ebaa2b89c5
Sankt Petersbug <sankt.petersbug@gmail.com>
2018-06-21 09:58:04 -0500

    added log statement to sub test cases

--- e2e/pwbook_test.go
+++ e2e/pwbook_test.go
@@ -23,25 +23,31 @@ func assertOutputContains(t *testing.T, output []byte, substr string) {
 func TestE2E(t *testing.T) {
     var out []byte
     var err error
+    const entryName = "entry1"
 
-    out, err = exec.Command("pwbook", "add", "entry1").Output()
+    out, err = exec.Command("pwbook", "add", entryName).Output()
     assertNoError(t, err)
-    assertOutputContains(t, out, "entry1")
+    assertOutputContains(t, out, entryName)
+    t.Logf("Added entry %q\n", entryName)
 
     out, err = exec.Command("pwbook", "list").Output()
     assertNoError(t, err)
     assertOutputContains(t, out, "entry1")
     assertOutputContains(t, out, "Total 1 entries")
+    t.Logf("Verified list containing the entry %q\n", entryName)
 
     out, err = exec.Command("pwbook", "update", "entry1").Output()
     assertNoError(t, err)
     assertOutputContains(t, out, "entry1")
+    t.Logf("Updated the entry %q\n", entryName)
 
     out, err = exec.Command("pwbook", "remove", "entry1").Output()
     assertNoError(t, err)
     assertOutputContains(t, out, "entry1")
+    t.Logf("Removed the entry %q\n", entryName)
 
     out, err = exec.Command("pwbook", "list").Output()
     assertNoError(t, err)
     assertOutputContains(t, out, "Total 0 entries")
+    t.Logf("Verified list not containing the entry %q\n", entryName)
 }
